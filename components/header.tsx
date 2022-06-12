826961847fec0a37e480756dc1635a43708b22a7
YongChenSu <yongchensu0@gmail.com>
2022-06-12 20:59:15 +0800

    feat: Update Header

--- components/header.tsx
+++ components/header.tsx
@@ -1,17 +1,19 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 const Header = () => {
-  return (
-    <h2 className="flex justify-between text-slate-50 text-xl md:text-2xl tracking-tight md:tracking-tighter leading-tight px-4 py-2 bg-black space-x-1">
-      <Link href="/">
-        <a className="">Dawning Blog</a>
-      </Link>
-      <div className="flex justify-around">
-        <div className="ml-3">程式文章</div>  
-        <div className="ml-3">程式雜記</div>  
-      </div>      
-    </h2>
-  )
-}
+    return (
+        <h2 className="bg-black flex justify-center">
+            <div className="flex justify-between text-slate-50 text-xl md:text-2xl tracking-tight md:tracking-tighter leading-tight px-4 py-2 max-w-7xl">
+                <Link href="/">
+                    <a className="">Dawning Blog</a>
+                </Link>
+                <div className="flex justify-around">
+                    <div className="ml-3">程式文章</div>
+                    <div className="ml-3">程式雜記</div>
+                </div>
+            </div>
+        </h2>
+    );
+};
 
-export default Header
+export default Header;
